216cf41bf50f4b6ca145ae48b04fab9c34dde749
Anand Otiv <w0858857@myscc.ca>
2025-04-12 18:39:34 -0400

    Completed controlling the animation stack

--- Animations/ContentView.swift
+++ Animations/ContentView.swift
@@ -10,22 +10,69 @@ import SwiftUI
 
 
 struct ContentView: View {
-    @State private var animationAmount = 0.0
+    @State private var enabled = false
 
     
+    
     var body: some View {
+        
+        
         Button("Tap Me") {
-            withAnimation (.spring(duration: 1, bounce: 0.5)){
-                animationAmount += 360
-            }
-        }
-        .padding(50)
-        .background(.red)
-        .foregroundStyle(.white)
-        .clipShape(.circle)
-        .rotation3DEffect(.degrees(animationAmount), axis: (x: 0, y: 1, z: 0))
+            enabled.toggle()
 
+        }
+        .frame(width: 200, height: 200)
+        .background(enabled ? .blue : .red)
+        .animation(nil, value: enabled)
+        .clipShape(.rect(cornerRadius: enabled ? 60 : 0))
+        //.animation(.default, value: enabled)
+        //.background(.blue)
+        .foregroundStyle(.white)
+        .clipShape(.rect(cornerRadius: enabled ? 60 : 0))
+        .animation(.default, value: enabled)
+
+
+
+        
+        
+        
+        //        Button("Tap Me") {
+//            // do nothing
+//        }
+//        .background(.blue)
+//        .frame(width: 200, height: 200)
+//        .foregroundStyle(.white)
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
     }
+
+    
+    
+    
+    //    @State private var animationAmount = 0.0
+//
+//    
+//    var body: some View {
+//        Button("Tap Me") {
+//            withAnimation (.spring(duration: 1, bounce: 0.5)){
+//                animationAmount += 360
+//            }
+//        }
+//        .padding(50)
+//        .background(.red)
+//        .foregroundStyle(.white)
+//        .clipShape(.circle)
+//        .rotation3DEffect(.degrees(animationAmount), axis: (x: 0, y: 1, z: 0))
+//
+//    }
 
 //    @State private var animationAmount = 1.0
 //    
@@ -102,4 +149,10 @@ struct ContentView: View {
 
 #Preview {
     ContentView()
+}
+
+struct Previews_ContentView_LibraryContent: LibraryContentProvider {
+    var views: [LibraryItem] {
+        LibraryItem(/*@START_MENU_TOKEN@*/Text("Hello, World!")/*@END_MENU_TOKEN@*/)
+    }
 }
