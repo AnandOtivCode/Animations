b574f6f19b44305a8699e01bd55912415ff17c9e
Anand Otiv <w0858857@myscc.ca>
2025-04-12 16:36:20 -0400

    Completed the steps for Implicit Animations

--- Animations/ContentView.swift
+++ Animations/ContentView.swift
@@ -7,16 +7,26 @@
 
 import SwiftUI
 
+
+
 struct ContentView: View {
+    @State private var animationAmount = 1.0
+
     var body: some View {
         Button("Tap Me") {
-            // do nothing
+            animationAmount += 1
+
         }
         .padding(50)
         .background(.red)
         .foregroundStyle(.white)
         .clipShape(.circle)
-   
+        .scaleEffect(animationAmount)
+        .animation(.default, value: animationAmount)
+        .blur(radius: (animationAmount - 1) * 3)
+
+
+
     }
 }
 
