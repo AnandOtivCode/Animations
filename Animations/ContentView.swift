810e81b28d70944a1194046ccd6390e7506759cb
Anand Otiv <w0858857@myscc.ca>
2025-04-12 17:33:10 -0400

    Completed Animating Bindings

--- Animations/ContentView.swift
+++ Animations/ContentView.swift
@@ -11,48 +11,76 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var animationAmount = 1.0
-
+    
     var body: some View {
-        Button("Tap Me") {
-           // animationAmount += 1
+        print(animationAmount)
 
+        return VStack {
+            Stepper("Scale amount", value: $animationAmount.animation(
+                .easeInOut(duration: 1)
+                    .repeatCount(3, autoreverses: true)
+            ), in: 1...10)
+            Spacer()
+            
+            Button("Tap Me") {
+                animationAmount += 1
+            }
+            .padding(40)
+            .background(.red)
+            .foregroundStyle(.white)
+            .clipShape(.circle)
+            .scaleEffect(animationAmount)
         }
-        .padding(50)
-        .background(.red)
-        .foregroundStyle(.white)
-        .clipShape(.circle)
-        //.scaleEffect(animationAmount)
-       // .blur(radius: (animationAmount - 1) * 3)
-        .overlay(
-            Circle()
-                .stroke(.red)
-                .scaleEffect(animationAmount)
-                .opacity(2 - animationAmount)
-                .animation(.easeOut(duration: 1)       //.repeatCount(3, autoreverses: true),
-                    .repeatForever(autoreverses: false),
-
-                //.delay(1),
-         value: animationAmount)
-        ).onAppear {
-            animationAmount = 2
-        }
-
-        //.animation(.default, value: animationAmount)
-        //.animation(.linear,value: animationAmount)
-//        .animation(.spring(duration: 1, bounce: 0.9), value: animationAmount)
-//        .animation(.easeInOut(duration: 1)       //.repeatCount(3, autoreverses: true),
-//            .repeatForever(autoreverses: true),
-//
-//        //.delay(1),
-// value: animationAmount)
-
-        
-
-        
-
-
-
     }
+    
+    
+    
+    
+    
+    
+    
+    //        Button("Tap Me") {
+    //           // animationAmount += 1
+    //
+    //        }
+    //        .padding(50)
+    //        .background(.red)
+    //        .foregroundStyle(.white)
+    //        .clipShape(.circle)
+    //        //.scaleEffect(animationAmount)
+    //       // .blur(radius: (animationAmount - 1) * 3)
+    //        .overlay(
+    //            Circle()
+    //                .stroke(.red)
+    //                .scaleEffect(animationAmount)
+    //                .opacity(2 - animationAmount)
+    //                .animation(.easeOut(duration: 1)       //.repeatCount(3, autoreverses: true),
+    //                    .repeatForever(autoreverses: false),
+    //
+    //                //.delay(1),
+    //         value: animationAmount)
+    //        ).onAppear {
+    //            animationAmount = 2
+    //        }
+    //
+    //        //.animation(.default, value: animationAmount)
+    //        //.animation(.linear,value: animationAmount)
+    ////        .animation(.spring(duration: 1, bounce: 0.9), value: animationAmount)
+    ////        .animation(.easeInOut(duration: 1)       //.repeatCount(3, autoreverses: true),
+    ////            .repeatForever(autoreverses: true),
+    ////
+    ////        //.delay(1),
+    //// value: animationAmount)
+    //
+    //
+    //
+    //
+    //
+    //
+    //
+    //    }
+    
+    
 }
 
 #Preview {
