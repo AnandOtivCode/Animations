3f96f71308cfffe94bb615f219edc530261b0370
Anand Otiv <w0858857@myscc.ca>
2025-04-12 16:21:46 -0400

    Initial Commit

---
+++ Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Anand Otiv on 2025-04-12.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
