e9ccd383d9aa4440308cc0161ad3bdf3ab9406c4
Anand Otiv <w0858857@myscc.ca>
2025-04-12 17:08:04 -0400

    Completed the steps for Customizing Animations

--- Animations/ContentView.swift
+++ Animations/ContentView.swift
@@ -14,16 +14,41 @@ struct ContentView: View {
 
     var body: some View {
         Button("Tap Me") {
-            animationAmount += 1
+           // animationAmount += 1
 
         }
         .padding(50)
         .background(.red)
         .foregroundStyle(.white)
         .clipShape(.circle)
-        .scaleEffect(animationAmount)
-        .animation(.default, value: animationAmount)
-        .blur(radius: (animationAmount - 1) * 3)
+        //.scaleEffect(animationAmount)
+       // .blur(radius: (animationAmount - 1) * 3)
+        .overlay(
+            Circle()
+                .stroke(.red)
+                .scaleEffect(animationAmount)
+                .opacity(2 - animationAmount)
+                .animation(.easeOut(duration: 1)       //.repeatCount(3, autoreverses: true),
+                    .repeatForever(autoreverses: false),
+
+                //.delay(1),
+         value: animationAmount)
+        ).onAppear {
+            animationAmount = 2
+        }
+
+        //.animation(.default, value: animationAmount)
+        //.animation(.linear,value: animationAmount)
+//        .animation(.spring(duration: 1, bounce: 0.9), value: animationAmount)
+//        .animation(.easeInOut(duration: 1)       //.repeatCount(3, autoreverses: true),
+//            .repeatForever(autoreverses: true),
+//
+//        //.delay(1),
+// value: animationAmount)
+
+        
+
+        
 
 
 
